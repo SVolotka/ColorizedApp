d6ab1b7db9409bcaf2de4d13de49fc5f19fbb2d8
Agro73 <s.volotka73@gmail.com>
2022-06-26 20:39:23 +0600

    Homework 2.04

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorizedView: UIView!
+   
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redSliderValue: UILabel!
+    @IBOutlet var greenSliderValue: UILabel!
+    @IBOutlet var blueSliderValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorizedView.layer.cornerRadius = 20
+    
+        redSlider.minimumTrackTintColor = .red
+       greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+       
+        redSliderAction()
+        greenSliderAction()
+        blueSliderAction()
     }
 
-
+    @IBAction func redSliderAction() {
+        redSliderValue.text = String(format: "%.2f", redSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
+        changeColor()
+    }
+     func changeColor() {
+        colorizedView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
 }
 
