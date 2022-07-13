8b20e4e94b6b4c5c753dcef7b39a44075725b2e5
Agro73 <s.volotka73@gmail.com>
2022-07-14 01:31:56 +0600

    Homework

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol SettingsViewControllerDelegate {
+    func setColor(color: UIColor)
+}
 
+class SettingsViewController: UIViewController {
+    
     @IBOutlet var colorizedView: UIView!
-   
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
@@ -19,35 +23,82 @@ class ViewController: UIViewController {
     @IBOutlet var greenSliderValue: UILabel!
     @IBOutlet var blueSliderValue: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        colorizedView.layer.cornerRadius = 20
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+  
+    var delegate: SettingsViewControllerDelegate!
+    var mainVC: UIColor!
     
-        redSlider.minimumTrackTintColor = .red
-       greenSlider.minimumTrackTintColor = .green
-        blueSlider.minimumTrackTintColor = .blue
-       
-        redSliderAction()
-        greenSliderAction()
-        blueSliderAction()
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    colorizedView.layer.cornerRadius = 20
+    setColor()
+    setValue(for: redSliderValue, greenSliderValue, blueSliderValue)
+
+        colorizedView.backgroundColor = mainVC
     }
 
-    @IBAction func redSliderAction() {
-        redSliderValue.text = String(format: "%.2f", redSlider.value)
-        changeColor()
+  
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
-    @IBAction func greenSliderAction() {
-        greenSliderValue.text = String(format: "%.2f", greenSlider.value)
-        changeColor()
+@IBAction func sliderAction(_ sender: UISlider) {
+    setColor()
+    switch sender {
+    case redSlider:
+        redSliderValue.text = string(from: redSlider)
+        redTextField.text = string(from: redSlider)
+    case greenSlider:
+        greenSliderValue.text = string(from: greenSlider)
+        greenTextField.text = string(from: greenSlider)
+    default:
+        blueSliderValue.text = string(from: blueSlider)
+        blueTextField.text = string(from: blueSlider)
+    }
+}
+
+    
+    @IBAction func doneButtonPressed() {
+        delegate?.setColor(color: colorizedView.backgroundColor ?? .red)
+        dismiss(animated: true)
     }
     
-    @IBAction func blueSliderAction() {
-        blueSliderValue.text = String(format: "%.2f", blueSlider.value)
-        changeColor()
+    
+    private func setColor() {
+    colorizedView.backgroundColor = UIColor(
+        red:
+            CGFloat(redSlider.value),
+        green:
+            CGFloat(greenSlider.value),
+        blue:
+            CGFloat(blueSlider.value),
+        alpha: 1
+    )
+}
+
+private func setValue(for labels: UILabel...) {
+    labels.forEach { label in
+        switch label {
+        case redSliderValue:
+            redSliderValue.text = string(from: redSlider)
+            redTextField.text = string(from: redSlider)
+        case greenSliderValue:
+            greenSliderValue.text = string(from: greenSlider)
+            greenTextField.text = string(from: greenSlider)
+        default:
+            blueSliderValue.text = string(from: blueSlider)
+            blueTextField.text = string(from: blueSlider)
+        }
     }
-     func changeColor() {
-        colorizedView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
-    }
+}
+
+private func string(from slider: UISlider) -> String {
+    String(format: "%.2f", slider.value)
+}
 }
 
---
+++ MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  ColorizedApp
+//
+//  Created by Sergei Volotka on 14.07.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let settingsVC = segue.destination as! SettingsViewController
+        settingsVC.delegate = self
+        settingsVC.mainVC = view.backgroundColor
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+    
+    extension MainViewController: SettingsViewControllerDelegate {
+    func setColor(color: UIColor) {
+        view.backgroundColor = color
+    }
+
+}
+
+
